1b554759c7690dd4227372367582c455528c8470
fots <facundotomatis@gmail.com>
2022-11-02 06:29:33 -0300

    added methods to remove from list

--- backend/src/main/java/com/ttps/backend/models/Emprendimiento.java
+++ backend/src/main/java/com/ttps/backend/models/Emprendimiento.java
@@ -1,5 +1,9 @@
 package com.ttps.backend.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 import javax.persistence.Basic;
@@ -12,11 +16,6 @@ import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Data
@@ -26,46 +25,80 @@ public class Emprendimiento {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     @Column(unique = true)
     private String domainUrl;
+
     private String nombre;
     private String descripcion;
     private double valorManguito;
     private Boolean filterByDonations;
     private Boolean filterByManguitos;
+
     @Lob
     @Basic(fetch = FetchType.LAZY)
     private byte[] imagen;
-    @OneToOne
-    private AppUser appUser;
+
     @ManyToMany(fetch = FetchType.LAZY)
     private List<Categoria> categorias;
-    @OneToMany
-    private List<Post> posts;
-    @OneToMany
-    private List<Plan> planes;
-    @OneToMany
-    private List<RedSocial> redesSociales;
-    @OneToMany
-    private List<Manguito> manguitos;
+
+    @OneToMany private List<Post> posts;
+    @OneToMany private List<Plan> planes;
+    @OneToMany private List<RedSocial> redesSociales;
+    @OneToMany private List<Manguito> manguitos;
 
     public void addCategoria(Categoria categoria) {
         this.categorias.add(categoria);
+    }
+
+    public void removeCategoria(Categoria categoria) {
+        this.categorias.stream()
+                .filter(p -> p.getId() == categoria.getId())
+                .findFirst()
+                .ifPresent(p -> this.categorias.remove(p));
     }
 
     public void addPost(Post post) {
         this.posts.add(post);
     }
 
+    public void removePost(Post post) {
+        this.posts.stream()
+                .filter(p -> p.getId() == post.getId())
+                .findFirst()
+                .ifPresent(p -> this.posts.remove(p));
+    }
+
     public void addPlan(Plan plan) {
         this.planes.add(plan);
+    }
+
+    public void removePlan(Plan plan) {
+        this.planes.stream()
+                .filter(p -> p.getId() == plan.getId())
+                .findFirst()
+                .ifPresent(p -> this.planes.remove(p));
     }
 
     public void addRedSocial(RedSocial redSocial) {
         this.redesSociales.add(redSocial);
     }
 
+    public void removeRedSocial(RedSocial redSocial) {
+        this.redesSociales.stream()
+                .filter(p -> p.getId() == redSocial.getId())
+                .findFirst()
+                .ifPresent(p -> this.redesSociales.remove(p));
+    }
+
     public void addManguito(Manguito manguito) {
         this.manguitos.add(manguito);
+    }
+
+    public void removeManguito(Manguito manguito) {
+        this.manguitos.stream()
+                .filter(p -> p.getId() == manguito.getId())
+                .findFirst()
+                .ifPresent(p -> this.manguitos.remove(p));
     }
 }
