03d3ff84c8d2751bf35d3ffc263cbd22ae6cb529
fots <facundotomatis@gmail.com>
2022-11-02 06:33:23 -0300

    creates role when app starts

--- backend/src/main/java/com/ttps/backend/BackendApplication.java
+++ backend/src/main/java/com/ttps/backend/BackendApplication.java
@@ -1,19 +1,30 @@
 package com.ttps.backend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.ttps.backend.models.Role;
+import com.ttps.backend.services.UserService;
+
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 
-	@Bean
-	BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    CommandLineRunner run(UserService userService) {
+        return args -> {
+            userService.saveRole(new Role(null, "ROLE_USER"));
+        };
+    }
 }
