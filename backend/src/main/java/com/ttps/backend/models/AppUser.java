73bc0c42d58bde06cb4c6b5cd65fa332bd0c6fe6
fots <facundotomatis@gmail.com>
2022-11-02 06:29:13 -0300

    added to user emprendimiento

--- backend/src/main/java/com/ttps/backend/models/AppUser.java
+++ backend/src/main/java/com/ttps/backend/models/AppUser.java
@@ -1,5 +1,9 @@
 package com.ttps.backend.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -10,27 +14,29 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import javax.persistence.OneToOne;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class AppUser {
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  private Long id;
-  @Column(unique = true)
-  private String email;
-  private String password;
-  private String fullName;
-  @ManyToMany(fetch = FetchType.EAGER)
-  private Collection<Role> roles = new ArrayList<>();
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
-  public void addRole(Role role) {
-    this.roles.add(role);
-  }
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+    private String fullName;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Collection<Role> roles = new ArrayList<>();
+
+    @OneToOne private Emprendimiento emprendimiento;
+
+    public void addRole(Role role) {
+        this.roles.add(role);
+    }
 }
