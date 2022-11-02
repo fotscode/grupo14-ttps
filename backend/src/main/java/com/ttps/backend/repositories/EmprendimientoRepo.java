63f29fb683fdd8a0ac0a19dd35b414b584bc1bb6
fots <facundotomatis@gmail.com>
2022-11-02 06:33:39 -0300

    changed findByDomainUrl to return Optional

--- backend/src/main/java/com/ttps/backend/repositories/EmprendimientoRepo.java
+++ backend/src/main/java/com/ttps/backend/repositories/EmprendimientoRepo.java
@@ -1,5 +1,7 @@
 package com.ttps.backend.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.ttps.backend.models.Emprendimiento;
 
 @Repository
 public interface EmprendimientoRepo extends JpaRepository<Emprendimiento, Long> {
-    Emprendimiento findByDomainUrl(String domainUrl);
+    Optional<Emprendimiento> findByDomainUrl(String domainUrl);
 }
