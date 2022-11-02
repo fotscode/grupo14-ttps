87f878053d8d12ee6ea016f823d7ca8f108eaaf9
fots <facundotomatis@gmail.com>
2022-11-02 06:32:01 -0300

    formatting

--- backend/src/main/java/com/ttps/backend/services/UserService.java
+++ backend/src/main/java/com/ttps/backend/services/UserService.java
@@ -7,8 +7,12 @@ import com.ttps.backend.models.Role;
 
 public interface UserService {
   AppUser saveUser(AppUser user);
+
   Role saveRole(Role role);
-  void addRoleToUser(String email,String roleName);
+
+  void addRoleToUser(String email, String roleName);
+
   AppUser getUser(String email);
+
   List<AppUser> getUsers();
 }
