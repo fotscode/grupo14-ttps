0914739b2464888479b96c734a3f3f4b78d270e8
fots <facundotomatis@gmail.com>
2022-12-08 03:23:55 -0300

    added material modules to ng modules

--- frontend/frontend/src/app/app.module.ts
+++ frontend/frontend/src/app/app.module.ts
@@ -12,6 +12,14 @@ import { ViewPostsComponentComponent } from './components/view-posts-component/v
 import { EditEmprendimientoComponentComponent } from './components/edit-emprendimiento-component/edit-emprendimiento-component.component';
 import { NavigationHeaderComponent } from './components/navigation-header/navigation-header.component';
 import { ContactFooterComponent } from './components/contact-footer/contact-footer.component';
+import { MaterialModule } from './material/material.module';
+import { LoginComponent } from './components/login/login.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TokenInterceptorService } from './services/token-interceptor.service';
+import { AuthGuard } from './guards/auth.guard';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [
@@ -22,14 +30,27 @@ import { ContactFooterComponent } from './components/contact-footer/contact-foot
     ViewPostsComponentComponent,
     EditEmprendimientoComponentComponent,
     NavigationHeaderComponent,
-    ContactFooterComponent
+    ContactFooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [
+    AuthGuard,
+    {
+      provide:HTTP_INTERCEPTORS,
+      useClass: TokenInterceptorService,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ frontend/frontend/src/app/material/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+const MaterialComponents = [
+  MatSnackBarModule,
+]
+
+@NgModule({
+  imports:[MaterialComponents],
+  exports:[MaterialComponents]
+})
+
+export class MaterialModule { }
