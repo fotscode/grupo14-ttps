ca36ea6627513440ac379319b3907536aacc304a
fots <facundotomatis@gmail.com>
2022-12-08 03:24:07 -0300

    added routing with guards

--- frontend/frontend/src/app/app-routing.module.ts
+++ frontend/frontend/src/app/app-routing.module.ts
@@ -1,16 +1,24 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { EmprendimientoComponentComponent } from './components/emprendimiento-component/emprendimiento-component.component';
-import { HomeComponentComponent } from './components/home-component/home-component.component';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { EmprendimientoComponentComponent } from './components/emprendimiento-component/emprendimiento-component.component'
+import { HomeComponentComponent } from './components/home-component/home-component.component'
+import { LoginComponent } from './components/login/login.component'
+import { AuthGuard } from './guards/auth.guard'
+import { LoggedInGuard } from './guards/logged-in.guard'
 
 const routes: Routes = [
-  {path:'Home', component: HomeComponentComponent},
-  {path:'miEmprendimiento', component:EmprendimientoComponentComponent},
-  {path:'**', redirectTo:'Home'}
-];
+  { path: 'Home', component: HomeComponentComponent },
+  {
+    path: 'miEmprendimiento',
+    component: EmprendimientoComponentComponent,
+    canActivate: [AuthGuard],
+  },
+  { path: 'login', component: LoginComponent, canActivate: [LoggedInGuard] },
+  { path: '**', redirectTo: 'Home' },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
