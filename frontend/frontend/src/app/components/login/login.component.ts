8c87fd129ab678aaca180c54dae5ee9f09710c19
fots <facundotomatis@gmail.com>
2022-12-08 03:23:14 -0300

    added login page and navigation header modifications using auth service

---
+++ frontend/frontend/src/app/components/login/login.component.ts
@@ -0,0 +1,40 @@
+import { Component, Inject, OnInit } from '@angular/core'
+import { MatSnackBar } from '@angular/material/snack-bar'
+import { Router } from '@angular/router'
+import { AuthService } from 'src/app/services/auth.service'
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  public constructor(
+    private authService: AuthService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {}
+
+  user = {
+    email: '',
+    password: '',
+  }
+
+  logIn() {
+    this.authService.signIn(this.user).subscribe(
+      (res) => {
+        localStorage.setItem('access_token', res.data.tokens.access_token)
+        localStorage.setItem('refresh_token', res.data.tokens.refresh_token)
+        this.snackBar.open('Se ha iniciado sesion', void 0, { duration: 3000 })
+        this.router.navigate(['/Home'])
+      },
+      (err) => {
+        this.snackBar.open('El email o contraseña es incorrecto', void 0, {
+          duration: 3000,
+        })
+        this.user.password = ''
+      }
+    )
+  }
+  ngOnInit(): void {}
+}
--- frontend/frontend/src/app/components/navigation-header/navigation-header.component.ts
+++ frontend/frontend/src/app/components/navigation-header/navigation-header.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { MatSnackBar } from '@angular/material/snack-bar'
+import { AuthService } from 'src/app/services/auth.service'
 
 @Component({
   selector: 'app-navigation-header',
   templateUrl: './navigation-header.component.html',
-  styleUrls: ['./navigation-header.component.css']
+  styleUrls: ['./navigation-header.component.css'],
 })
 export class NavigationHeaderComponent {
+  constructor(
+    private authService: AuthService,
+    private snackBar: MatSnackBar
+  ) {}
 
+  isLogged() {
+    return this.authService.loggedIn()
+  }
+
+  logOut() {
+    this.authService.logOut()
+    this.snackBar.open('Se ha cerrado sesion', void 0, { duration: 3000 })
+  }
 }
