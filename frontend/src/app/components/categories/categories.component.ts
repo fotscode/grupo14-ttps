c6964fb9b8d8ee73a0d98ab0f87e6ceafa472c46
fots <facundotomatis@gmail.com>
2022-12-10 06:46:49 -0300

    added categories functionality and styles

--- frontend/src/app/components/categories/categories.component.ts
+++ frontend/src/app/components/categories/categories.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { MatSnackBar } from '@angular/material/snack-bar'
+import { Categoria } from 'src/app/interfaces/Categoria'
+import { CategoriesService } from 'src/app/services/categories.service'
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.css']
+  styleUrls: ['./categories.component.css'],
 })
 export class CategoriesComponent {
+  categories: Categoria[] = []
 
+  constructor(
+    private categoriesService: CategoriesService,
+    private snackBar: MatSnackBar
+  ) {}
+
+  ngOnInit(): void {
+    this.getCategories()
+  }
+
+  getCategories() {
+    this.categoriesService.getCategories().subscribe((res: any) => {
+      this.categories = res.data.categorias
+    })
+  }
+
+  saveCategory(category: Categoria) {
+    this.categoriesService.saveCategories(category).subscribe((res: any) => {
+      this.getCategories()
+      this.snackBar.open(res.message, void 0, { duration: 3000 })
+    })
+  }
+
+  addEmptyCategory() {
+    this.categories.push({ nombre: '', color: '' })
+  }
+
+  deleteCategory(id: number | undefined) {
+    if (id) {
+      this.categoriesService.deleteCategory(id).subscribe((res: any) => {
+        this.snackBar.open(res.message, void 0, { duration: 3000 })
+        this.getCategories()
+      })
+    }
+  }
 }
---
+++ frontend/src/app/services/categories.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { CategoriasResponse } from '../interfaces/responses/CategoriasResponse';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriesService {
+  private URL = environment.baseApiUrl
+
+  constructor(private http: HttpClient) { }
+
+  getCategories(){
+    return this.http.get<CategoriasResponse>(this.URL+"/categoria/list")
+  }
+
+  saveCategories(category:any){
+    return this.http.put<CategoriasResponse>(this.URL+"/categoria/update",category)
+  }
+
+  deleteCategory(id:number){
+    return this.http.delete<CategoriasResponse>(this.URL+"/categoria/delete/"+id)
+  }
+}
