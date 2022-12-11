9e3500b42396ecff82ae25182e57bddaa294445f
fots <facundotomatis@gmail.com>
2022-12-11 07:34:07 -0300

    added route for getting empredimiento using jwt

--- backend/src/main/java/com/ttps/backend/controllers/EmprendimientoController.java
+++ backend/src/main/java/com/ttps/backend/controllers/EmprendimientoController.java
@@ -1,15 +1,11 @@
 package com.ttps.backend.controllers;
 
-import com.ttps.backend.models.Emprendimiento;
-import com.ttps.backend.models.Response;
-import com.ttps.backend.services.implementations.EmprendimientoServiceImpl;
-
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-
-import lombok.RequiredArgsConstructor;
+import java.time.LocalDateTime;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,14 +15,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-import java.util.Map;
+import com.ttps.backend.models.Emprendimiento;
+import com.ttps.backend.models.Response;
+import com.ttps.backend.services.EmprendimientoService;
+import com.ttps.backend.services.UserService;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/api/emprendimiento")
 @RequiredArgsConstructor
+@Slf4j
 public class EmprendimientoController {
-    private final EmprendimientoServiceImpl emprendimientoService;
+    private final EmprendimientoService emprendimientoService;
+    private final UserService userService;
 
     @GetMapping("/list")
     public ResponseEntity<Response> getEmprendimientos() {
@@ -38,6 +42,22 @@ public class EmprendimientoController {
                         .status(HttpStatus.OK)
                         .statusCode(HttpStatus.OK.value())
                         .path("/api/emprendimiento/list")
+                        .build());
+    }
+
+    @GetMapping("/get")
+    public ResponseEntity<Response> getEmprendimiento() {
+        String user = SecurityContextHolder.getContext().getAuthentication().getName();
+        log.info("Emprendimiento de {}", userService.getUser(user));
+        Long id = userService.getUser(user).getEmprendimiento().getId();
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .data(Map.of("emprendimiento", emprendimientoService.get(id)))
+                        .message("Emprendimiento retornado")
+                        .status(HttpStatus.OK)
+                        .statusCode(HttpStatus.OK.value())
+                        .path("/api/emprendimiento/get")
                         .build());
     }
 
