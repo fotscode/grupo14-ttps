774648671c63a6be34cdf9f4fa3fcf249155fc22
fots <facundotomatis@gmail.com>
2022-12-11 08:05:58 -0300

    now grid is working properly and added undefined check in response

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,28 +1,32 @@
-import { Component } from '@angular/core';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import { Emprendimiento } from 'src/app/interfaces/Emprendimiento';
-import { EmprendimientosService } from 'src/app/services/emprendimientos.service';
+import { Component } from '@angular/core'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { Emprendimiento } from 'src/app/interfaces/Emprendimiento'
+import { EmprendimientosService } from 'src/app/services/emprendimientos.service'
 
 @Component({
   selector: 'app-home-component',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-    faSearch=faSearch;
-    emprendimientos: Emprendimiento[]=[]
-    loading=true
-    index=0
+  faSearch = faSearch
+  emprendimientos: Emprendimiento[] = []
+  loading = true
+  index = 0
 
-    constructor(private emprendimientosService: EmprendimientosService){}
+  constructor(private emprendimientosService: EmprendimientosService) {}
 
-    ngOnInit():void{
-      this.emprendimientosService.getEmprendimientos().subscribe((res)=>{
-        this.emprendimientos=res.data.emprendimientos
+  ngOnInit(): void {
+    this.emprendimientosService.getEmprendimientos().subscribe(
+      (res) => {
+        if (res.data.emprendimientos)
+          this.emprendimientos = res.data.emprendimientos
         console.log(res)
-        this.loading=false
-      },(err)=>{
+        this.loading = false
+      },
+      (err) => {
         console.log(err)
-      })
-    }
+      }
+    )
+  }
 }
